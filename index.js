c31c1ccfddd524521a22e7da64d63b928fa1c531
vishalchoudhary47 <63789813+vishalchoudhary47@users.noreply.github.com>
2021-07-23 13:08:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,76 +1,62 @@
-const fact = () => {
-   let num = document.getElementById("input").value;
+const fact = (num) => {
 let fact = 1;
     for(i=1;i<=num;i++) {
         fact *=i;
     }
-    // console.log(`factorial of ${num} is`+ ": "+ fact);
-    // document.write(`<h2>factorial of ${num} is: ${fact}</h2>`);
-    document.getElementById("result").value = fact;
+    console.log(`1) factorial of ${num} is`+ ": "+ fact);
+    console.log(" ");
     }
 
-// fact(5);
+fact(5);
 
-const largestNum = () => {
-    let a,b,c;
-    a = document.getElementById("a").value;
-    b = document.getElementById("b").value;
-    c = document.getElementById("c").value;
+const largestNum = (a,b,c) => {
+    console.log(`2) largest of three No's ${a} ${b} ${c}`);
+
     if(a>b){
         if(a>c){
-            // console.log(`The largest Number is: ${a}`);
-            document.getElementById("result1").value=a;
+            console.log(`The largest Number is: ${a}`);
         }else {
-            // console.log(`The largest Number is: ${c}`);
-            document.getElementById("result1").value=c;
+            console.log(`The largest Number is: ${c}`);
         }
     }else if(b>c){
-        // console.log(`The largest Number is: ${b}`);
-        document.getElementById("result1").value=b;
+        console.log(`The largest Number is: ${b}`);
     }
     else {
-        // console.log(`The largest Number is: ${c}`);
-        document.getElementById("result1").value=c;
+        console.log(`The largest Number is: ${c}`);
     }
+    console.log(" ");
 }
-// largestNum(155,246,159);
+largestNum(155,246,159);
 
-const SumNaturalNo = () => {
-    let num = document.getElementById('nat').value;
+const SumNaturalNo = (num) => {
     let sum=0;
     for(i=1;i<=num;i++){
         sum = sum+i;
     }
-    // console.log(`Sum of Natural No's of 1 to 50 is: ${sum}`);
-    document.getElementById('result2').value = sum;
+    console.log(`3) Sum of Natural No's of 1 to ${num} is: ${sum}`);
+    console.log(" ");
 }
-// SumNaturalNo();
+SumNaturalNo(10);
 
-const FibbnonicSequence = () => {
-    let num = document.getElementById("fib").value;
-    // console.log("Fibbnonic Series upto 8 terms..")
+const FibbnonicSequence = (num) => {
+    console.log("4) Fibbnonic Series upto 8 terms..")
     let a=0,b=1,next;
-    // console.log(a);
-    // console.log(b);
-    document.write( `Fibbnonic Series upto ${num} terms:  `);
-    document.write(` ${a}`);
-    document.write(` ${b}`);
+    console.log(a);
+    console.log(b);
     for(i=2;i<num;i++) {
         next=a+b;
-        // console.log(next);
-        // document.getElementById('result3').value = next;
-        document.write(` ${next}`);
+        console.log(next);
         a=b;
         b=next;
     }
+    console.log(" ");
 }
-// FibbnonicSequence(8);
+FibbnonicSequence(8);
 
 
-const triangel = () => {
-    let b,h;
-    b = document.getElementById('b1').value;
-    h = document.getElementById('h').value;
+const triangel = (b,h) => {
     let area = (1/2)*b*h;
-    document.getElementById('result3').value = area;
+    console.log(`5) area of triangel with base= ${b} and Height = ${h} is equal: ${area}`);
+    console.log(" ");
 }
+triangel(4,5);
